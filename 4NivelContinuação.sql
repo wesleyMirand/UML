ff26e48db631c9d0172357375e8dbcba14911e3c
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-08-30 14:01:09 -0300

    Update 4NivelContinuação.sql

--- 4NivelContinuação.sql
+++ 4NivelContinuação.sql
@@ -2,7 +2,7 @@ Preso a TecNologia MySql (Dialeto SqL)
 
 create table `produto`(
  `id` int(11) not null auto_increment;
- `descrição` var char(255) default null;
+ `descrição` varchar(255) default null;
  `preço` float default null;
  primary key(`ìd`)
  ) engine = innodb default charset = latin1;
