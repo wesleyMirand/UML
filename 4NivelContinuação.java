8ba840c02d2cb3396f0042c07b1daaad565d4fa7
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-08-30 14:10:03 -0300

    Create 4NivelContinuação.java

---
+++ 4NivelContinuação.java
@@ -0,0 +1,15 @@
+//Preso a Tecnologia Java (Orientada a Objetos)\\
+
+Public class Produto {
+  private Integer id;
+  private String descrição;
+  private Double preço;
+  
+  Public Produto ( Integer id , String descrição , Double preço) {
+    this.id = id;
+    this.descrição= descrição;
+    this.preço = preço ;
+   }
+}
+
+(...)
